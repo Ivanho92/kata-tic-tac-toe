124393daec52fd087c0c9ab1a3464f99563c96b5
Ivan Rodrigues <ivan.rodrigues@expleogroup.com>
2022-12-12 12:48:50 +0100

    Added: CORS configuration to allow front-end app to fetch data

--- src/main/java/com/ivan_rodrigues/kata/tictactoe/Application.java
+++ src/main/java/com/ivan_rodrigues/kata/tictactoe/Application.java
@@ -2,6 +2,11 @@ package com.ivan_rodrigues.kata.tictactoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -11,5 +16,8 @@ public class Application {
 
 		System.out.println("Server is listening on http://localhost:8081...");
 	}
+
+
+
 
 }
